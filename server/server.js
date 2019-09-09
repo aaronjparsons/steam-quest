e732769f7ec668018ca47cd44ffe54131c739051
aaronjparsons <aaron.parsons@live.ca>
2019-09-08 21:35:53 -0600

    Setup server cors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const steam = require('./steam-helper')
 const Game = require('./db/models/game')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-const port = 3000
+
+const corsOptions = {
+  origin: process.env.CLIENT_ORIGIN
+}
+app.use(cors(corsOptions))
 
 mongoose
   .connect(process.env.DB_URL, {
@@ -80,4 +85,5 @@ app.post('/markGameCompleted', async (req, res) => {
   })
 })
 
+const port = 3000
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
