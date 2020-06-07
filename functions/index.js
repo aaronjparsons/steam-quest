a11024d5d6120a743ff6ac0c0ebdef3c04031b0f
aaronjparsons <aaron.parsons@live.ca>
2020-06-07 00:45:25 -0600

    Set up cloud functions base with test route

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const express = require('express')
+const bodyParser = require("body-parser")
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+//initialize firebase inorder to access its services
+admin.initializeApp(functions.config().firebase)
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!")
+//initialize express server
+const app = express()
+
+//add the path to receive request and set json as bodyParser to process the body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//initialize the database and the collection
+const db = admin.firestore()
+
+// Create new user
+app.post('/users', async (req, res) => {
+  try {
+      const user = {
+        firstName: req.body['firstName'],
+        lastName: req.body['lastName']
+      }
+
+    const newDoc = await db.collection('users').add(user)
+    res.status(201).send(`Created a new user: ${newDoc.id}`)
+  } catch (error) {
+    res.status(400).send(`User should cointain firstName, lastName!`)
+  }
 })
+
+//define google cloud function name
+exports.api = functions.https.onRequest(app)
