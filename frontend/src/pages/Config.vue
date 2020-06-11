36e6dd7c857c25d92a08c5124d6b2f31c993039a
aaronjparsons <aaron.parsons@live.ca>
2020-06-10 22:14:47 -0600

    Add cors to express app. Setup some very simple initial user creation

--- frontend/src/pages/Config.vue
+++ frontend/src/pages/Config.vue
@@ -1,25 +1,78 @@
 <template>
   <div>
-    <p>User</p>
+    <h3>Steam-Quest Configuration</h3>
+    <div class="config-container">
+      <b-loading :active="userRequestLoading" :is-full-page="false" />
+      <b-field label="Steam Profile ID">
+        <b-input v-model="user.steamId"></b-input>
+      </b-field>
+      <b-button type="is-primary" :loading="configFormLoading" @click="submitConfigForm">Save</b-button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data() {
+    return {
+      user: {},
+      newUser: false,
+      userRequestLoading: false,
+      configFormLoading: false
+    }
+  },
+
   mounted() {
-    window.Twitch.ext.onAuthorized(auth => {
-      window.Twitch.ext.rig.log('channelId: ', auth.channelId)
-      window.Twitch.ext.rig.log('token: ', auth.token)
-      window.Twitch.ext.rig.log('userId: ', auth.userId)
+    window.Twitch.ext.onAuthorized(async (auth) => {
+      console.log('channelId: ', auth.channelId)
+      console.log('token: ', auth.token)
+      console.log('userId: ', auth.userId)
+
+      await this.getUser(auth.userId)
+
+      if (!Object.keys(this.user).length) {
+        this.newUser = true
+        this.user = {
+          id: auth.userId,
+          steamId: ''
+        }
+      }
     })
 
     window.Twitch.ext.onContext(context => {
       window.Twitch.ext.rig.log('mode: ', context.mode)
     })
+  },
+
+  methods: {
+    async getUser(id) {
+      this.userRequestLoading = true
+      try {
+        const response = await axios.get(`https://us-central1-steam-quest-ed9fd.cloudfunctions.net/api/users/${id}`)
+        this.user = response.data
+      } catch (error) {
+        console.error(error)
+      }
+      this.userRequestLoading = false
+    },
+
+    async submitConfigForm() {
+      this.configFormLoading = true
+      try {
+        await axios.post('https://us-central1-steam-quest-ed9fd.cloudfunctions.net/api/users', this.user)
+      } catch (error) {
+        console.log(error)
+      }
+      this.configFormLoading = false
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.config-container {
+  position: relative;
+}
 </style>
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require("body-parser")
 
 //initialize firebase inorder to access its services
@@ -9,6 +10,8 @@ admin.initializeApp(functions.config().firebase)
 //initialize express server
 const app = express()
 
+app.use(cors())
+
 //add the path to receive request and set json as bodyParser to process the body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,19 +19,37 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //initialize the database and the collection
 const db = admin.firestore()
 
-// Create new user
+// Get broadcaster (config)
+app.get('/users/:userId', async (req, res) => {
+  const userId = req.params.userId
+
+  try {
+    const user = await db.collection('users').doc(userId).get()
+
+    if (user.exists) {
+      res.status(200).json({
+        id:user.id,
+        data:user.data()
+      })
+    } else {
+      res.status(200).json({})
+    }
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+// Save broadcaster (config)
 app.post('/users', async (req, res) => {
   try {
-      const user = {
-        firstName: req.body['firstName'],
-        lastName: req.body['lastName']
-      }
-
-    const newDoc = await db.collection('users').add(user)
-    res.status(201).send(`Created a new user: ${newDoc.id}`)
+    await db.collection('users').doc(req.body.id).set({
+      steamId: req.body.steamId
+    })
+    res.status(201).send()
   } catch (error) {
-    res.status(400).send(`User should cointain firstName, lastName!`)
+    res.status(500).send(error)
   }
+
 })
 
 //define google cloud function name
